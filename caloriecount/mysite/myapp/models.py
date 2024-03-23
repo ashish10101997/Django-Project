3b068d80272e2dab510cfd16479b98372be2e310
Ashish <yadavashish4890@gmail.com>
2024-03-23 11:11:45 +0530

    Second Commit

---
+++ caloriecount/mysite/myapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+ 
+class Food(models.Model):
+ 
+    def __str__(self):
+        return self.name
+ 
+    name = models.CharField(max_length=100)
+    carbs = models.FloatField()
+    protein = models.FloatField()
+    fats = models.FloatField()
+    calories = models.IntegerField()
+ 
+# Food consumed by user. Storing th Food model into food_consumed. N number of user willuse this account so user is created.
+class Consume(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    food_consumed = models.ForeignKey(Food,on_delete=models.CASCADE)
